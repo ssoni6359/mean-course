fae12f4ff1770b3ab6e365dbec934513c42775d5
Shubham Soni <ssoni6359@gmail.com>
2024-03-07 08:42:33 +0530

    3. string interpolation - way to output content

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,7 +5,9 @@ import { Component } from "@angular/core";
     templateUrl: './post-create.component.html' 
 })
 export class PostCreateComponent {
+    newPost = '';
+
     onAddPost() {
-        alert('Posts Added');
+        this.newPost = 'The users\'s post';
     }
 }
